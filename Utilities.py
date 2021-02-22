73e874a49a10d6a14c0f7fb19851dfe18af7afa2
Daniel Davis <dda49@uclive.ac.nz>
2021-02-23 01:24:43 +1300

    Improved dubugging

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 def print_name(f):
     
     def inner():
+    
+        logging.info(f"Function call: {f.__name__}")        
         print(f.__name__)
         f()
         
